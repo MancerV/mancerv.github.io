dc9ae13a4746798d051d4b56f4dbd1bdfc771127
MancerV <karltamparong28@gmail.com>
2025-11-12 16:10:17 -0600

    Add files via upload

--- lab7/js/script.js
+++ lab7/js/script.js
@@ -1,1 +1,15 @@
+function init(){
+//add your javascrip between these two lines of code
+ 
+const alertMe = document.getElementById("entrybutton");
+const input = document.getElementById("entryinput");
+const output = document.getElementById("textoutput");
 
+alertMe.addEventListener("click", function () {
+  const words = input.value;
+  output.textContent = words;
+  alert("Karl:  " + words);
+  });
+}
+
+window.addEventListener('load', init);
